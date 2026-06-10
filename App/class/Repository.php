414c1c41a06afae8872a6246c4df6fdca748da6f
Ahaya <yohan.hellec@outlook.fr>
2026-06-10 21:57:03 +0200

    Suppression fantome App

--- App/class/Repository.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\class;
-
-class Repository extends AbstractEntity{
-    
-}
--- App/vue/back/backoffice_accueil.php
+++
@@ -1,8 +0,0 @@
-<section id="choice">
-    <button class="fondVertClair"><a href="?path=first_new_event">Créer un événement</a></button>
-    <button class="fondVertClair"><a href="?path=first_new_itinerary">Créer un itinéraire</a></button>
-    <?php if (!empty($_SESSION) && ($_SESSION['user']->get('role') > 2)): ?>
-        <button class="fondVertSombre"><a href="#">Modération</a></button>
-    <?php endif ?>
-
-</section>
